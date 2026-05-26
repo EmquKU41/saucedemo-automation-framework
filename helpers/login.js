4e7e236b60388aa75d48df9e9f408fd7a12d8235
EmquKU41 <maksymilianbrudka1@gmail.com>
2026-05-26 04:17:38 +0100

    Add README screenshots

--- helpers/login.js
+++ helpers/login.js
@@ -2,7 +2,7 @@
 const { expect } = require('@playwright/test');
 
 async function login(page) {
-  await page.goto('https://www.saucedemo.com/');
+  await page.goto('https://www.saucedemo.com');
   await page.fill('#user-name', 'standard_user');
   await page.fill('#password', 'secret_sauce');
   await page.click('#login-button');
